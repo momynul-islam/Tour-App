297a3a1c6622a7fa1d0528c6f7024ef384099465
momynul-islam <islammomynul.cod@gmail.com>
2024-09-18 12:43:46 +0600

    fix the static directory folder name public to Public

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 
 // Global Middlewares
 app.use(cors());
