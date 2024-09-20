c7287444de0ed0d1f4603aa4c47d247f25aa175d
momynul-islam <islammomynul.cod@gmail.com>
2024-09-20 14:56:54 +0600

    this.from option value fixed in email.js file

--- Utils/email.js
+++ Utils/email.js
@@ -8,7 +8,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(" ")[0];
     this.url = url;
-    this.from = `Tour App <no-reply@islammomynul.cod@gmail.com}>`;
+    this.from = `Tour App <no-reply@islammomynul.cod@gmail.com>`;
   }
 
   newTransport() {
